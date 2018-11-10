dd66ba1f419ba812bc76e4a1de1292615aa8c850
apriyanblog29 <39805263+apriyanblog29@users.noreply.github.com>
2018-11-10 17:25:25 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,29 @@
+<?php
+$android_id = "7c82ad7057369d79";
+$deviceCode = "354921078623864";
+$device_ip  = "114.4.78.250";
+$token = "7d62FfijHQIvn0CnZLYJ8xxxxxxxxxxxxxxxxxx";
+$tk = "ACE7U3JgE_KIlcBb2pSYSqxEkHz8CSTiXK5xdHRodw";
+$uuid = "87d47bb3365c4af7acf34798aa8a5e0d";
+$userid = "7501331";
+$sign = "4cec16b45234c4b5d794f755378dbee4";
+
+####################################################
+#|note:
+#| *untuk aktifitas nuyul multi akun
+#|  gunakan andoid_id yang berbeda setiap akun
+#|  gunakan devicecode (imei) yang berbeda setiap akun
+#|  gunakan device_ip yang berbeda setiap akun
+#| *untuk settingan lainya wajib sama 
+#|  dengan data dari packet capture
+#|
+####################################################
+#| cara mengaktifkan multi akun
+#| duplikat data di atas dari anroid_id => sign
+#| buat file baru dengan nama bebas terserah kamu
+#| aktifkan skrip di bawah dngn menghapus tanda pagar
+#| jika skrip di aktifkan maka hapus data di atas 
+#|#####################################################
+#| hapus pagar di bawah untuk mengaktifkan skrip
+
+# include(readline("masukan config: "));
